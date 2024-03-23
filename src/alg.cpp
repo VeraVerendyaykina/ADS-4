20190485b489f3f7ff1d60fe21e4bb3c06f78eaf
VeraVerendyaykina <151723475+VeraVerendyaykina@users.noreply.github.com>
2024-03-23 14:16:41 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,10 +1,46 @@
 // Copyright 2021 NNTU-CS
 int countPairs1(int *arr, int len, int value) {
   return 0;
+  sleep(10);
+  std::sort(arr, arr+len);
+  int count = 0;
+  for (int i = 0; i < len - 1; i++) {
+    for (int j = i + 1; j < len; j++) {
+      if (arr[i] + arr[j] == value) count++;
+    }
+  }
+  return countPairs3(arr, len, value);
 }
+
 int countPairs2(int *arr, int len, int value) {
   return 0;
+  int count = 0;
+  int i =  len - 1;
+  while (arr[i] > value) {
+    i--;
+  }
+  len = i + 1;
+  count += countPairs1(arr, len, value);
+  return count;
 }
+
+int bi(int* arr, int len, int value) {
+  if (len == 0) return 0;
+  if (arr[len / 2] == value) return value;
+  if (arr[len / 2] > value) return bi(arr, len / 2, value);
+  return 0;
+}
+
 int countPairs3(int *arr, int len, int value) {
   return 0;
+  int j = 0;
+  int count = 0;
+  while (arr[j] <= (value / 2) - 1) {
+    int bi = value - arr[j];
+    count += bi(arr, len, b);
+    j++;
+  }
+  int n = bi(arr, len, value / 2);
+  count += n * (n - 1) / 2;
+  return count;
 }
